ff6eb319b99129ed497deb50a078e61ddc41209c
George Vincent Peña <gpena8826@gmail.com>
2023-11-04 08:50:58 +0800

    newly adjusted

--- pages/_appShell.tsx
+++ pages/_appShell.tsx
@@ -146,7 +146,7 @@ export default function FullLayout() {
             <AppShell
                 header={{ height: 60 }}
                 aside={{
-                    width: 150,
+                    width: 200,
                     breakpoint: "md",
                     collapsed: { desktop: false, mobile: !opened },
                 }}
